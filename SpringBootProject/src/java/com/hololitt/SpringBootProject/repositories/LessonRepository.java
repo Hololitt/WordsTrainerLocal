055baeea28ffba940d65ff1ab4913c62be3f3528
Nikon <109473540+Hololitt@users.noreply.github.com>
2025-10-26 23:18:17 +0100

    Create LessonRepository.java

---
+++ SpringBootProject/src/java/com/hololitt/SpringBootProject/repositories/LessonRepository.java
@@ -0,0 +1,10 @@
+package com.hololitt.SpringBootProject.repositories;
+
+import com.hololitt.SpringBootProject.models.Lesson;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LessonRepository extends JpaRepository<Lesson, Integer> {
+    Lesson findByTitle(String title);
+}
