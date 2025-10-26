7ebc5af9de51fd46a3477ef3ff6aa8eefa82945e
Nikon <109473540+Hololitt@users.noreply.github.com>
2025-10-26 23:08:32 +0100

    Create LanguageCardBlockRepository.java

---
+++ SpringBootProject/src/java/com/hololitt/SpringBootProject/repositories/LanguageCardBlockRepository.java
@@ -0,0 +1,12 @@
+package com.hololitt.SpringBootProject.repositories;
+
+import com.hololitt.SpringBootProject.models.LanguageCardsBlock;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LanguageCardBlockRepository extends JpaRepository<LanguageCardsBlock, Long> {
+LanguageCardsBlock findByTypeAndUserId(String typ, long userId);
+void deleteByTypeAndUserId(String typ, long userId);
+boolean existsByTypeAndUserId(String type, long userId);
+}
