4a8bbc3a7f48fcb45ac8ad656ab7207b12735cd3
Nikon <109473540+Hololitt@users.noreply.github.com>
2025-10-26 21:43:31 +0100

    Delete src directory

--- src/main/java/com/hololitt/SpringBootProject/enums/TrainingType.java
+++
@@ -1,7 +0,0 @@
-package com.hololitt.SpringBootProject.enums;
-
-public enum TrainingType {
-    MIX,
-    WORD_TO_TRANSLATION,
-    TRANSLATION_TO_WORD;
-}
--- src/main/java/com/hololitt/SpringBootProject/exceptions/LanguageCardNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.hololitt.SpringBootProject.exceptions;
-
-public class LanguageCardNotFoundException extends Exception{
-    public LanguageCardNotFoundException(String message){
-        super(message);
-    }
-}
